907084d807ac43e5ba58863cacc5936c0a77be30
K M Riaz Uddin <96685003+kmriazuddin@users.noreply.github.com>
2022-10-01 20:27:43 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+const searchFun = () =>{
+    let filter = document.getElementById('myInput').value.toUpperCase();
+    let myTable = document.getElementById('myTable');
+    let tr = myTable.getElementsByTagName('tr');
+    for(var i = 0; i < tr.length; i++){
+        let td = tr[i].getElementsByTagName('td')[1];
+        if(td){
+            let textvalue = td.textContent || td.innerHTML;
+            if(textvalue.toUpperCase().indexOf(filter) > -1){
+                tr[i].style.display = "";
+            }else{
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
